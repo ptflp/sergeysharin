effad51aa1a3b9d7e5c7f9b6bb8147653caac509
gudicon <gudicon@gmail.com>
2016-11-18 11:29:21 +0900

    Theme added

---
+++ header.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package Weeble
+ * @subpackage Sergey Sharyn
+ * @since Sergey Sharyn 1.0
+ */
+
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<title>Официальный сайт фотографа Сергей Шарин, Sergey Sharin</title>
+
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="../favicon.ico">
+
+	<!-- LOADING FONTS AND ICONS -->
+	<link href="http://fonts.googleapis.com/css?family=Raleway:900,600,400" rel="stylesheet" property="stylesheet" type="text/css" media="all">
+
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/pe-icon-7-stroke.css">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/font-awesome.min.css">
+
+	<!-- REVOLUTION STYLE SHEETS -->
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/settings.css">
+	<!-- REVOLUTION LAYERS STYLES -->
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/layers.css">
+	<!-- REVOLUTION NAVIGATION STYLES -->
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/navigation.css">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/style.css">
+	<?php wp_head(); ?>
+</head>
+<body>
+<?php body_class(); ?>
